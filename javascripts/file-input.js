c716e31ca845279b88287e7e398ba8a3c8cad9e5
Anish Ramaswamy <anishlr@users.noreply.github.com>
2016-05-16 16:53:06 -0700

    Placeholder to display the missing gearbit info

--- javascripts/file-input.js
+++ javascripts/file-input.js
@@ -9,17 +9,35 @@ var SAVE_GEARBIT_FIELD_ID = "0="
 var SAVE_COLLECTIBLES_FIELD_DELIMITER = ">"
 var SAVE_GEARBIT_FIELD_DELIMITER = "&"
 
+var GEARBIT_INFO_HTML_TEMPLATE = '<input id="centralTown" type="radio" name="tab-group" checked="checked" />' +
+    '<label for="centralTown">Central Town</label>' +
+    '<input id="north" type="radio" name="tab-group" />' +
+    '<label for="north">North Zone</label>' +
+    '<input id="south" type="radio" name="tab-group" />' +
+    '<label for="south">South Zone</label>' +
+    '<input id="east" type="radio" name="tab-group" />' +
+    '<label for="east">East Zone</label>' +
+    '<input id="west" type="radio" name="tab-group" />' +
+    '<label for="west">West Zone</label>' +
+    '<div id="content">' +
+    '<div id="centralTownContent"> <p>Central</p> </div>' +
+    '<div id="northContent"> <p>North</p> </div>' +
+    '<div id="southContent"> <p>South</p> </div>' +
+    '<div id="eastContent"> <p>East</p> </div>' +
+    '<div id="westContent"> <p>West</p> </div>' +
+    '</div>'
+
 ;
 (function(document, window, index) {
-    var inputFileControl = document.getElementById('inputFile');
-    inputFileControl.addEventListener('change', function(e) {
-      // There should only be one file since the input element doesn't allow for selecting multiple files
-      var selectedFile = this.files[0];
-      if (selectedFile) {
-        var reader = new FileReader();
-        reader.onload = parseSaveContents;
-        reader.readAsText(selectedFile);
-      }
+    var inputFile = document.getElementById('inputFile');
+    inputFile.addEventListener('change', function(e) {
+        // There should only be one file since the input element doesn't allow for selecting multiple files
+        var selectedFile = this.files[0];
+        if (selectedFile) {
+            var reader = new FileReader();
+            reader.onload = parseSaveContents;
+            reader.readAsText(selectedFile);
+        }
     });
 }(document, window, 0));
 
@@ -42,16 +60,16 @@ function parseSaveContents(e) {
     var gearbitFieldStartIndex = collectibles.indexOf(SAVE_GEARBIT_FIELD_ID) + SAVE_GEARBIT_FIELD_ID.length;
     var gearbitString = collectibles.substring(gearbitFieldStartIndex, collectibles.indexOf(SAVE_COLLECTIBLES_FIELD_DELIMITER, gearbitFieldStartIndex));
     var gearbits = gearbitString.split(SAVE_GEARBIT_FIELD_DELIMITER);
-    
+
     var gearbitInfoWrapperControl = document.getElementById("gearbitInfoWrapper");
     var uploadButtonControl = document.getElementById("uploadButton");
-    
+
     gearbitInfoWrapperControl.removeAttribute("contentInvisible");
     gearbitInfoWrapperControl.className = "content";
-    
+
     uploadButtonControl.removeAttribute("content");
     uploadButtonControl.className = "contentInvisible";
-    
+
     var gearbitInfoControl = document.getElementById("gearbitInfo");
-    gearbitInfoControl.innerHTML = gearbits;
+    gearbitInfoControl.innerHTML = GEARBIT_INFO_HTML_TEMPLATE;
 }
