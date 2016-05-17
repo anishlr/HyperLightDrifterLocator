19d80fdb6ffa7d4f40339a4f6a20e5fd542267bd
Anish Ramaswamy <anishlr@users.noreply.github.com>
2016-05-16 17:36:33 -0700

    Cleaned up code a bit
    Don't display zone tabs in which all gearbits have already been collected.
    
    Indicate if all gearbits have been collected.

--- javascripts/file-input.js
+++ javascripts/file-input.js
@@ -9,23 +9,31 @@ var SAVE_GEARBIT_FIELD_ID = "0="
 var SAVE_COLLECTIBLES_FIELD_DELIMITER = ">"
 var SAVE_GEARBIT_FIELD_DELIMITER = "&"
 
-var GEARBIT_INFO_HTML_TEMPLATE = '<input id="centralTown" type="radio" name="tab-group" checked="checked" />' +
-    '<label for="centralTown">Central Town</label>' +
-    '<input id="north" type="radio" name="tab-group" />' +
-    '<label for="north">North Zone</label>' +
-    '<input id="south" type="radio" name="tab-group" />' +
-    '<label for="south">South Zone</label>' +
-    '<input id="east" type="radio" name="tab-group" />' +
-    '<label for="east">East Zone</label>' +
-    '<input id="west" type="radio" name="tab-group" />' +
-    '<label for="west">West Zone</label>' +
-    '<div id="content">' +
-    '<div id="centralTownContent"> <p>Central</p> </div>' +
-    '<div id="northContent"> <p>North</p> </div>' +
-    '<div id="southContent"> <p>South</p> </div>' +
-    '<div id="eastContent"> <p>East</p> </div>' +
-    '<div id="westContent"> <p>West</p> </div>' +
-    '</div>'
+var GEARBIT_INFO_ALL_COLLECTED_HTML = '<p id="allCollectedP">All gearbits collected!</p>'
+var GEARBIT_INFO_CONTENT_WRAPPER_HTML_START = '<div id="content">'
+var GEARBIT_INFO_CONTENT_WRAPPER_HTML_END = '</div>'
+var GEARBIT_INFO_HTML = {
+    "centralTown": {
+        "tab": '<input id="centralTownTab" type="radio" name="tab-group" checked="checked" /> <label for="centralTownTab">Central Town</label>',
+        "content": '<div id="centralTownContent"> <p>Central</p> </div>'
+    },
+    "north": {
+        "tab": '<input id="northTab" type="radio" name="tab-group" /> <label for="northTab">North Zone</label>',
+        "content": '<div id="northContent"> <p>North</p> </div>'
+    },
+    "south": {
+        "tab": '<input id="southTab" type="radio" name="tab-group" /> <label for="southTab">South Zone</label>',
+        "content": '<div id="southContent"> <p>South</p> </div>'
+    },
+    "east": {
+        "tab": '<input id="eastTab" type="radio" name="tab-group" /> <label for="eastTab">East Zone</label>',
+        "content": '<div id="eastContent"> <p>East</p> </div>'
+    },
+    "west": {
+        "tab": '<input id="westTab" type="radio" name="tab-group" /> <label for="westTab">West Zone</label>',
+        "content": '<div id="westContent"> <p>West</p> </div>'
+    }
+}
 
 ;
 (function(document, window, index) {
@@ -59,10 +67,21 @@ function parseSaveContents(e) {
     var collectibles = save[SAVE_COLLECTIBLES_IDENTIFIER];
     var gearbitFieldStartIndex = collectibles.indexOf(SAVE_GEARBIT_FIELD_ID) + SAVE_GEARBIT_FIELD_ID.length;
     var gearbitString = collectibles.substring(gearbitFieldStartIndex, collectibles.indexOf(SAVE_COLLECTIBLES_FIELD_DELIMITER, gearbitFieldStartIndex));
-    var gearbits = gearbitString.split(SAVE_GEARBIT_FIELD_DELIMITER);
 
+    showGearbitInfo(gearbitString.split(SAVE_GEARBIT_FIELD_DELIMITER));
+}
+
+function showGearbitInfo(gearbits) {
+    console.log(gearbits);
     var gearbitInfoWrapperControl = document.getElementById("gearbitInfoWrapper");
     var uploadButtonControl = document.getElementById("uploadButton");
+    var gearbitInfoControl = document.getElementById("gearbitInfo");
+    var central = [];
+    var north = [];
+    var south = [];
+    var east = [];
+    var west = [];
+    var gearbitInfoHtml = "";
 
     gearbitInfoWrapperControl.removeAttribute("contentInvisible");
     gearbitInfoWrapperControl.className = "content";
@@ -70,6 +89,42 @@ function parseSaveContents(e) {
     uploadButtonControl.removeAttribute("content");
     uploadButtonControl.className = "contentInvisible";
 
-    var gearbitInfoControl = document.getElementById("gearbitInfo");
-    gearbitInfoControl.innerHTML = GEARBIT_INFO_HTML_TEMPLATE;
+    if (central.length > 0) {
+        gearbitInfoHtml += GEARBIT_INFO_HTML["centralTown"]["tab"];
+    }
+    if (north.length > 0) {
+        gearbitInfoHtml += GEARBIT_INFO_HTML["north"]["tab"];
+    }
+    if (south.length > 0) {
+        gearbitInfoHtml += GEARBIT_INFO_HTML["south"]["tab"];
+    }
+    if (east.length > 0) {
+        gearbitInfoHtml += GEARBIT_INFO_HTML["east"]["tab"];
+    }
+    if (west.length > 0) {
+        gearbitInfoHtml += GEARBIT_INFO_HTML["west"]["tab"];
+    }
+    if (gearbitInfoHtml.length > 0) {
+        gearbitInfoHtml += GEARBIT_INFO_CONTENT_WRAPPER_HTML_START;
+        if (central.length > 0) {
+            gearbitInfoHtml += GEARBIT_INFO_HTML["centralTown"]["content"];
+        }
+        if (north.length > 0) {
+            gearbitInfoHtml += GEARBIT_INFO_HTML["north"]["content"];
+        }
+        if (south.length > 0) {
+            gearbitInfoHtml += GEARBIT_INFO_HTML["south"]["content"];
+        }
+        if (east.length > 0) {
+            gearbitInfoHtml += GEARBIT_INFO_HTML["east"]["content"];
+        }
+        if (west.length > 0) {
+            gearbitInfoHtml += GEARBIT_INFO_HTML["west"]["content"];
+        }
+        gearbitInfoHtml += GEARBIT_INFO_CONTENT_WRAPPER_HTML_END;
+    } else {
+        gearbitInfoHtml = GEARBIT_INFO_ALL_COLLECTED_HTML;
+    }
+
+    gearbitInfoControl.innerHTML = gearbitInfoHtml;
 }
